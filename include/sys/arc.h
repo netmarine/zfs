f28add3446a6ef3a22f6967f64b56165c822bed2
George Amanakis <gamanakis@gmail.com>
2020-02-24 16:23:04 -0500

    Fix rebuild after off/onlining if l2arc_vdev_present
    Signed-off-by: George Amanakis <gamanakis@gmail.com>

--- include/sys/arc.h
+++ include/sys/arc.h
@@ -310,6 +310,7 @@ void arc_fini(void);
 void l2arc_add_vdev(spa_t *spa, vdev_t *vd, boolean_t rebuild);
 void l2arc_remove_vdev(vdev_t *vd);
 boolean_t l2arc_vdev_present(vdev_t *vd);
+void l2arc_rebuild_vdev(vdev_t *vd, boolean_t rebuild);
 void l2arc_init(void);
 void l2arc_fini(void);
 void l2arc_start(void);
