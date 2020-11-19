1adfdfbb2c80eb9b2b5140bb04bfb8f45c1e7f8f
George Amanakis <gamanakis@gmail.com>
2020-11-19 20:05:45 +0100

    leave only useraccounting
    Signed-off-by: George Amanakis <gamanakis@gmail.com>

--- module/os/freebsd/zfs/zio_crypt.c
+++ module/os/freebsd/zfs/zio_crypt.c
@@ -1083,8 +1083,6 @@ zio_crypt_do_objset_hmacs(zio_crypt_key_t *key, void *data, uint_t datalen,
 	    osp->os_groupused_dnode.dn_type == DMU_OT_NONE) ||
 	    (datalen <= OBJSET_PHYS_SIZE_V1) ||
 	    (intval & OBJSET_FLAG_USERACCOUNTING_COMPLETE) == 0 ||
-	    (intval & OBJSET_FLAG_USEROBJACCOUNTING_COMPLETE) == 0 ||
-	    (intval & OBJSET_FLAG_PROJECTQUOTA_COMPLETE) == 0) {
 		bzero(local_mac, ZIO_OBJSET_MAC_LEN);
 		return (0);
 	}
